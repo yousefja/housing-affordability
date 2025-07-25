86c696557c6ba2496d05026574e441d70ecc7be1
Yousef <yousefja@buffalo.edu>
2025-07-25 17:48:28 -0400

    checkpoint - finished affordability analysis

--- src/affordability_analysis.py
+++ src/affordability_analysis.py
@@ -13,7 +13,7 @@ from util import format_price
 
 PATH_TO_HOUSING_DATA = '../output/scraped_listings.csv'
 PATH_TO_INCOME_DATA = "../data/ACSST5Y2023.S1901_2025-07-24T192912/ACSST5Y2023.S1901-Data.csv"
-BUFFALO_ZIPS = "14201, 14202, 14203, 14204, 14206, 14207, 14208, 14209, 14210, 14211, 14212, 14213, 14214, 14215, 14216, 14218, 14220, 14222, 14223, 14225, 14226, 14227, 14221, 14224, 14228, 14217, 14425, 14219"
+# BUFFALO_ZIPS = "14201, 14202, 14203, 14204, 14206, 14207, 14208, 14209, 14210, 14211, 14212, 14213, 14214, 14215, 14216, 14218, 14220, 14222, 14223, 14225, 14226, 14227, 14221, 14224, 14228, 14217, 14425, 14219"
 
 
 df_housing = pd.read_csv(PATH_TO_HOUSING_DATA)
@@ -29,15 +29,19 @@ df_housing['Bedrooms'] = pd.to_numeric(df_housing['Bedrooms'], errors='coerce')
 df_housing['Bathrooms'] = pd.to_numeric(df_housing['Bathrooms'], errors='coerce')
 df_housing['SqFt'] = df_housing['SqFt'].replace('[,]', '', regex=True).astype(float)
 
+# calculate cost of house by sqft
+df_housing['Price_Per_SqFt'] = df_housing['Price'] / df_housing['SqFt']
+
 # get zip code from geography col
 df_income['Zipcode'] = df_income['Geographic Area Name'].apply(lambda x: int(x.split()[-1].strip()))
 
 # get buffalo zips as list of ints
-buffalo_zips = list(map(int, ast.literal_eval(BUFFALO_ZIPS)))
+# buffalo_zips = list(map(int, ast.literal_eval(BUFFALO_ZIPS)))
 
+# no longer needed, filtering happens when joining to the scraped listing data
 # filter census income data for buffalo zips
-df_income = df_income[df_income.Zipcode.isin(buffalo_zips)]
-assert(len(df_income) == len(buffalo_zips))
+# df_income = df_income[df_income.Zipcode.isin(buffalo_zips)]
+# assert(len(df_income) == len(buffalo_zips))
 
 # select only columns of interest from income data
 df_income = df_income[['Estimate!!Households!!Median income (dollars)',
@@ -47,6 +51,9 @@ df_income = df_income[['Estimate!!Households!!Median income (dollars)',
 # rename median income column
 df_income.rename(columns={'Estimate!!Households!!Median income (dollars)':'Household_Median_Income'}, inplace=True)
 
+# convert median income to float
+df_income['Household_Median_Income'] = df_income['Household_Median_Income'].astype(float)
+
 ###################
 # MERGE INCOME DATA
 ###################
@@ -55,6 +62,7 @@ df_income.rename(columns={'Estimate!!Households!!Median income (dollars)':'House
 df_housing['Zipcode'] = df_housing['Zipcode'].astype(int)
 df_analysis = df_housing.merge(df_income, on='Zipcode', how='left')
 
+# TODO:maybe don't filter by buffalo and just have the join to the scraped listing data do the filtering?
 # get rows from df_housing that didn't join
 # df_no_join = df_analysis[df_analysis['Estimate!!Households!!Median income (dollars)'].isna()]
 # df_no_join.Zipcode.unique() # get missing zip codes from listings
@@ -75,8 +83,8 @@ do not exceed 30% of a household's gross income. This threshold is established b
 '''
 
 # gap between affordable price and actual price
-df_analysis['Affordable_Price'] = df_analysis.Household_Median_Income * 3
-df_analysis['Affordability_Gap'] = df_analysis.Affordable_Price - 
+df_analysis['Affordable_Price'] = df_analysis.Household_Median_Income * 3.0
+df_analysis['Affordability_Gap'] = df_analysis.Price - df_analysis.Affordable_Price 
 
 
 '''
@@ -90,25 +98,42 @@ Affordable to ≤50% AMI → "very low income housing"
 
 
 
-
-
 # ZIP LEVEL METRICS
 
+
+def zipcode_aggregates(df, df_income):
+    '''
+    HPI divides the median house price by the median household income. 
+    While a good HPI is generally considered to be between 2 and 3, this can vary significantly 
+    based on local economic conditions and housing availability.
+    '''
+    
+    # get min, max, and median house price per zipcode
+    df_zip_agg = df.groupby('Zipcode').agg(
+        Min_Price=('Price', 'min'),
+        Max_Price=('Price', 'max'),
+        Median_Price=('Price', 'median'),
+    ).reset_index()
+
+    # get zip median income 
+    df_zip_agg = df_zip_agg.merge(df_income,
+                                  how='left',
+                                  on='Zipcode')
+    
+    # calculate HPI per zip
+    df_zip_agg['HPI'] = df_zip_agg.Median_Price / df_zip_agg.Household_Median_Income
+    
+    # flag zips where lowest house price is > 3x the median income
+    df_zip_agg['Unaffordable'] = df_zip_agg.Min_Price > df_zip_agg.Household_Median_Income*3
+    
+    return df_zip_agg
+
+
+df_zip_level_analysis = zipcode_aggregates(df_analysis, df_income)
+
+
 '''
-Another metric, the HPI divides the median house price by the median household income. 
-While a good HPI is generally considered to be between 2 and 3, this can vary significantly 
-based on local economic conditions and housing availability.
-'''
-group by zip, calculate HPI per zip
-
-# affordability ratio: price-to-income
-df_analysis['PIR'] = df_analysis['Price'] / 
-
-cost-burden index
-
-
-
-From cgpt
+IF I WANT TO EXPAND LATER -- FOR FURTHER INDIVIDUAL LEVEL AFFORDABILITY METRICS 
 
 # Example: Single home and AMI
 home_price = 250000
@@ -139,32 +164,4 @@ for level, pct in [('Extremely Low Income', 0.3), ('Very Low Income', 0.5),
     income = ami * pct
     affordable = is_affordable(home_price, income, interest_rate, loan_term_years, tax_insurance_rate)
     print(f"{level} (AMI {int(pct*100)}%): {'Affordable' if affordable else 'Not Affordable'}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
---
+++ src/config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 25 17:43:21 2025
+
+@author: youse
+"""
+
+# for scraper
+PATH_TO_CITY_LISTINGS = "https://www.redfin.com/city/2832/NY/Buffalo"
+PATH_TO_LISTINGS_OUTPUT = "../output/scraped_listings.csv"
+MAX_LISTINGS = 300
+
+# for affordability analaysis
+PATH_TO_HOUSING_DATA = '../output/scraped_listings.csv'
+PATH_TO_INCOME_DATA = "../data/input/ACSST5Y2023.S1901_2025-07-24T192912/ACSST5Y2023.S1901-Data.csv"
